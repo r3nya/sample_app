bd16f2b0ae12fd3da4d5fcbc9cd25d5e6f6104dc
Andrew M <me@r3nya.ru>
2013-04-06 12:17:59 +0400

    + aplication helper & new rpec test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,12 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App")
+    end
+
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title', :text => "#{base_title} | Home")
@@ -22,6 +28,12 @@ describe "Static pages" do
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "should have the title 'Help'" do
@@ -37,6 +49,12 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'About Us')
     end
 
+    it "should have the base title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App")
+    end
+
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title', :text => "#{base_title} | About Us")
@@ -48,6 +66,12 @@ describe "Static pages" do
     it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "should have the title 'Contact'" do
