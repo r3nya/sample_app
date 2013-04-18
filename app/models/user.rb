cfe1cbe92c678eac062ec0aa35fb5a7808533df3
Andrew M <me@r3nya.ru>
2013-04-19 00:19:38 +0400

    has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   before_save { email.downcase! }
   before_save :create_remember_token
